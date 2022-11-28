9327a0d9bdc90cda5fa675b4ba0385e1d0f256bb
Octavio Chagas Junior <86677802+OctavioJunior@users.noreply.github.com>
2022-11-28 19:39:42 -0300

    :sparkles: feat: Add create client function

--- backend/Resolvers/ClientResolver.ts
+++ backend/Resolvers/ClientResolver.ts
@@ -1,9 +1,27 @@
-import { Query, Resolver } from "type-graphql"
+import { Arg, Mutation, Query, Resolver } from "type-graphql";
+import { CreateClientInput } from "../database/InputDB/ClientInput";
+import { ClientMongo } from "../database/SchemaDB/Client";
+import { Client } from "./../Models/Client";
 
-Resolver()
-export class ClientResolver{
-  @Query(() => String)
-  async hello() {
-  return "Hello World!"
+Resolver();
+export class ClientResolver {
+  @Query(() => [Client])
+  async clients() {
+    return await ClientMongo.find();
+  }
+
+  @Mutation(() => Client)
+  async createClient(
+    @Arg("createClientObject") createClientObject: CreateClientInput
+  ) {
+    const { name, email, cpf, adress, tel } = createClientObject;
+
+    return await ClientMongo.create({
+      name,
+      email,
+      cpf,
+      adress,
+      tel,
+    });
   }
 }
---
+++ backend/database/SchemaDB/Client.ts
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+const schema = new mongoose.Schema({
+  name: String,
+  email: String,
+  cpf: String,
+  adress: String,
+  tel: String,
+});
+
+export const ClientMongo = mongoose.model("Client", schema);
